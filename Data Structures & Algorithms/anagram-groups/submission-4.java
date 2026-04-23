add23c26705c2651b8bcd163ce974b2ea8149be2
Arlan Prado <arlanjprado@gmail.com>
2026-04-22 21:01:57 -0500

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // Using a map, use the key as the sorted word to compare then add the word to the list in the value
+        Map<String, List<String>> anagramMap = new HashMap<>();
+        for(int i = 0; i < strs.length; i++){
+            String comparedWord = strs[i];
+            String reSorted = reSort(comparedWord);
+            anagramMap.computeIfAbsent(reSorted, k -> new ArrayList<>()).add(comparedWord);
+        }
+        return new ArrayList<>(anagramMap.values());
+
+    }
+    private String reSort(String originalString) {
+        char[] charArray = originalString.toCharArray();
+        Arrays.sort(charArray);
+        return new String(charArray);
+    }
+}   
