f681e28619ca3351f3f336687db8b8d13799d2bd
Arlan Prado <arlanjprado@gmail.com>
2026-04-25 22:12:28 -0500

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        List<Character> stack = new ArrayList<>();
+        for( char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.addLast(c);
+            }
+                if (checkCloseBrackets(c, ')', '(', stack)) {
+                    return false;
+                } 
+                if (checkCloseBrackets(c, ']', '[', stack)) {
+                    return false;
+                } 
+                if (checkCloseBrackets(c, '}', '{', stack)) {
+                    return false;
+                } 
+        }
+        if (!stack.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+    private boolean checkCloseBrackets(char input, char close, char open, List<Character> stack) {
+        return input == close && (stack.isEmpty() || stack.removeLast() != open);
+    }
+}
