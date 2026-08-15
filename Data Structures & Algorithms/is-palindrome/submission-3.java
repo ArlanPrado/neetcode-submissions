00deaf98963619bb6113a52a7b1bc076ee0a063b
Arlan Prado <arlanjprado@gmail.com>
2026-08-15 18:58:06 -0500

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String newString = "";
+        for(char c : s.toCharArray()) {
+            c = Character.toLowerCase(c);
+            if(Character.isLetter(c)) {
+                newString += c;
+            }
+        }
+
+        s = newString;
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right) {
+            System.out.println("" + s.charAt(left) + " " +s.charAt(right));
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            ++left;
+            --right;
+        }
+        return true;
+    }
+}
