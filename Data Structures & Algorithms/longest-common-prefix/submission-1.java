1aa38917e8f9a8cb9666264acb46d95a9cdb6c4e
Arlan Prado <arlanjprado@gmail.com>
2026-08-17 20:11:39 -0500

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        // loop through the array
+        // after each iteration we should have at least a letter
+        // if the last letter looked doesn't match the letter in the same position then return whatever is in the result
+
+        String prefix = "";
+        char letter;
+        boolean canIterate = true;
+        int charAt = 0;
+        int wordAt = 0;
+        while (charAt < strs[0].length()) {
+            letter = strs[0].charAt(charAt);
+            for (int i = 1; i < strs.length; i++) {
+                if (strs[i].length() < charAt) {
+                    return prefix;
+                }
+                if (letter != strs[i].charAt(charAt)) {
+                    return prefix;
+                }
+            }
+            prefix += letter;
+            ++charAt;
+        }
+        return prefix;
+    }
+}
